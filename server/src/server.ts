3ce868dc8755a5577768699123ddf159edc23275
Madan Kumar <kumarmadan.j@gmail.com>
2020-08-09 12:42:35 +0530

    Added eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const PORT = 8000;
+const PORT = 8081;
 const app = express();
 
 app.get('/echo', (req, res) => {
     res.send('Echo From server');
 });
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log(`Server started at port : ${PORT}`);
 });
